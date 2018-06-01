de55d9523eeebf782f5b9ca5c2e61a9815dff6cc
Paul Scott <paul.scott@ravelin.com>
2018-06-01 13:50:16 +0100

    Version 0.0.3

--- ravelin.js
+++ ravelin.js
@@ -9,7 +9,7 @@
   }
 }(typeof self !== 'undefined' ? self : this, function () {
 
-  var version = '0.0.2';
+  var version = '0.0.3';
 
   var RSAKey = (function(){
     // prng4.js - uses Arcfour as a PRNG
--- test/test.js
+++ test/test.js
@@ -60,7 +60,7 @@ describe('ravelinjs', function() {
                     c.cardCiphertext != "" && c.cardCiphertext.length > 10 &&
                     c.aesKeyCiphertext != "" && c.aesKeyCiphertext.length > 10 &&
                     c.algorithm == "RSA_WITH_AES_256_GCM" &&
-                    c.ravelinjsVersion == "0.0.2"
+                    c.ravelinjsVersion == "0.0.3"
                 );
             }
 
