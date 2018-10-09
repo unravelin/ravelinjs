a38ae3f0745c657f17cae679b3cae4e3bb1aadf2
sHesl <sHesl@users.noreply.github.com>
2018-10-09 10:25:56 +0100

    Add support for public RSA key index to support key rotations

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect;
 const dummyRSAKey = '10001|BB2D2D2FD3812FEBECF8955843228A0E1952342583DFC02B8393475C414E16FDCBE8753BD63C164104785D8A67E344D495B5C0C622CE8D643F3191BC6BE0D3050F391F77E1D7E1B8F69DA34B308477E31F775CCC44158E33FD7DDD51AC87DD33AD80B9B1BF850CEC79A189F011C0689C36C0C91BF6DB9CD65BB7399710E32D9876C00DD44103E54A64A44BF427E1BA4F48DA7AF3D623DBCCF282ED8D4CCAE31B921A9BE92F9E8D7B5C50FBD89D828539CAE3E3493D4F6D7ADA19A876D9DF3801B5C3CFFA6A3C72A246150F307D789BAD6E2408DA5EF05EE805E11C133FFEDFA57CD1C35E49106ADDAC43C51995B9C318066C9ACB4042D8A534370D79F1BAD601';
+const dummyRSAKeyWithIndex = '2|10001|BB2D2D2FD3812FEBECF8955843228A0E1952342583DFC02B8393475C414E16FDCBE8753BD63C164104785D8A67E344D495B5C0C622CE8D643F3191BC6BE0D3050F391F77E1D7E1B8F69DA34B308477E31F775CCC44158E33FD7DDD51AC87DD33AD80B9B1BF850CEC79A189F011C0689C36C0C91BF6DB9CD65BB7399710E32D9876C00DD44103E54A64A44BF427E1BA4F48DA7AF3D623DBCCF282ED8D4CCAE31B921A9BE92F9E8D7B5C50FBD89D828539CAE3E3493D4F6D7ADA19A876D9DF3801B5C3CFFA6A3C72A246150F307D789BAD6E2408DA5EF05EE805E11C133FFEDFA57CD1C35E49106ADDAC43C51995B9C318066C9ACB4042D8A534370D79F1BAD601';
 
 describe('ravelinjs', function() {
     var ravelin;
@@ -13,7 +14,8 @@ describe('ravelinjs', function() {
             const err = 'Invalid value provided to RavelinJS.setRSAKey';
             expect(() => ravelin.setRSAKey(123)).to.throw(err);
             expect(() => ravelin.setRSAKey('derp')).to.throw(err);
-            expect(() => ravelin.setRSAKey('10001|asdf|wrong-format')).to.throw(err);
+            expect(() => ravelin.setRSAKey('10001|asdf|wrong-format|heh')).to.throw(err);
+            expect(() => ravelin.setRSAKey('10001')).to.throw(err);
         });
     });
 
@@ -54,54 +56,55 @@ describe('ravelinjs', function() {
 
         it('generates ciphers', function() {
             ravelin.setRSAKey(dummyRSAKey);
-            function validCipher(c) {
-                c = JSON.parse(c);
-                return (
-                    c.methodType == 'paymentMethodCipher' &&
-                    c.cardCiphertext != '' && c.cardCiphertext.length > 10 &&
-                    c.aesKeyCiphertext != '' && c.aesKeyCiphertext.length > 10 &&
-                    c.algorithm == 'RSA_WITH_AES_256_GCM' &&
-                    c.ravelinjsVersion == '0.0.9'
-                );
-            }
 
-            expect(ravelin.encrypt({
+            expect(JSON.parse(ravelin.encrypt({
                 pan: '4111 1111 1111 1111',
                 month: 10,
                 year: 2020,
-            })).to.satisfy(validCipher);
+            }))).to.satisfy(validateCipher);
 
-            expect(ravelin.encrypt({
+            expect(JSON.parse(ravelin.encrypt({
                 pan: '4111 1111 1111 1111',
                 month: '10',
                 year: '20',
-            })).to.satisfy(validCipher);
+            }))).to.satisfy(validateCipher);
 
-            expect(ravelin.encrypt({
+            expect(JSON.parse(ravelin.encrypt({
                 pan: '4111 1111 1111 1111',
                 month: '12',
                 year: '20',
-            })).to.satisfy(validCipher);
+            }))).to.satisfy(validateCipher);
         });
 
         it('can return payload as object', function() {
             ravelin.setRSAKey(dummyRSAKey);
-            function validCipher(c) {
-                return (
-                    c.methodType == 'paymentMethodCipher' &&
-                    c.cardCiphertext != '' && c.cardCiphertext.length > 10 &&
-                    c.aesKeyCiphertext != '' && c.aesKeyCiphertext.length > 10 &&
-                    c.algorithm == 'RSA_WITH_AES_256_GCM' &&
-                    c.ravelinjsVersion == '0.0.9'
-                );
-            }
 
             expect(ravelin.encryptAsObject({
                 pan: '4111 1111 1111 1111',
                 month: 10,
                 year: 2020,
-            })).to.satisfy(validCipher);
+            })).to.satisfy(validateCipher);
+        });
+
+        it('can parse key index and include in payload', function() {
+          ravelin.setRSAKey(dummyRSAKeyWithIndex);
+          result = ravelin.encryptAsObject({
+              pan: '4111 1111 1111 1111',
+              month: 10,
+              year: 2020,
+          });
+
+          expect(result.keyIndex).to.eq(2);
         });
     });
 
 });
+
+function validateCipher(c) {
+  return c.methodType == 'paymentMethodCipher' &&
+         c.cardCiphertext != '' && c.cardCiphertext.length > 10 &&
+         c.aesKeyCiphertext != '' && c.aesKeyCiphertext.length > 10 &&
+         c.algorithm == 'RSA_WITH_AES_256_GCM' &&
+         c.ravelinjsVersion == '0.0.10' &&
+         typeof(c.keyIndex) === 'number';
+}
