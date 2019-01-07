19c05a6e56fe4de8537695e3cb0289b76f17feba
sHesl <sHesl@users.noreply.github.com>
2019-01-07 12:49:25 +0000

    e2e tests

--- test/base.conf.js
+++ test/base.conf.js
@@ -133,7 +133,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 30000,
+        timeout: 60000,
     },
 
     // Test reporter for stdout.
--- test/specs/e2e-test.js
+++ test/specs/e2e-test.js
@@ -1,60 +1,157 @@
-describe('ravelinjs returns the encrypted card details', function() {
-    const rsaKey = process.env.E2E_RSA_KEY;
-    const nameOnCard = process.env.E2E_NAME_ON_CARD;
-    const outputFilepath = process.env.E2E_CIPHERTEXT_FILE;
-    console.log('Name on card:', nameOnCard);
-    console.log('Ciphertext destination:', outputFilepath);
-    console.log('RSA Key:', rsaKey);
-    if (!rsaKey.match(/^[^|]+\|[^|]+$/)) throw new Error("Envvar E2E_RSA_KEY must be set and of the form ...|...");
-    if (!nameOnCard) throw new Error("Envvar E2E_NAME_ON_CARD must be set.");
-    if (!outputFilepath) throw new Error("Envvar E2E_CIPHERTEXT_FILE must be set.");
+// e2e tests are triggered from inside the Ravelin 'integration-test-service' microservice.
+// They test that the values generated/submitted from the ravelinjs library behave as expected when sent
+// to the Ravelin API. We achieve this by calling ravelinjs functions, writing their outputs to dedicated
+// text files (persisted as 'Artifacts' to the build in CircleCI) and then making assertions against their
+// values from inside the integration-test-service.
+describe('ravelinjs', function() {
 
-    it('loads the page', function() {
-        browser.url('/pages/scripttag/index.html');
-    });
+  // We have hardcoded the file names of our outputs because they must be individually specified as
+  // 'store_artifacts' commands in circle.yml. These files will be written to the root dir of the repo,
+  // and available at the path 'home/circleci/repo/$.txt' via the API.
+  const ciphertextOutputFile = './cipher.txt';
+  const deviceIdFile = './deviceid.txt';
+  const sessionIdFile = './sessionid.txt';
 
-    it('sets up the RSA key', function() {
-        // Set the key.
-        const demoKeyMsg = browser.getText('#output');
-        browser.setValue('#rsa-key', rsaKey);
-        browser.click('#key-form input[type=submit]');
+  // When the integration test is launched, some env variables are also set, some of which are unique per run.
+  // These must also all be provided in order to complete this test suite. See the RavelinJS integration test
+  // to see how these values are generated and how they are provided to the job.
+  const publicApiToken = process.env.E2E_PUBLIC_API_TOKEN;
+  const rsaKey = process.env.E2E_RSA_KEY;
+  const nameOnCard = process.env.E2E_NAME_ON_CARD;
+  const customerId = process.env.E2E_CUSTOMER_ID;
+  const orderId = process.env.E2E_ORDER_ID;
 
-        // Check there wasn't an error.
-        raise(browser.getText('#output-error'));
+  if (!publicApiToken || !publicApiToken.match(/(publishable_key_live_)([0-z]+)/)) {
+    throw new Error("Envvar E2E_PUBLIC_API_TOKEN must be set and of the form publishable_key_live_XXXXXXXX");
+  }
+  if (!rsaKey || !rsaKey.match(/^[^|]+\|[^|]+$/)) {
+    throw new Error("Envvar E2E_RSA_KEY must be set and of the form ...|...");
+  }
 
-        // Confirm the key has been updated.
-        const output = browser.getText('#output');
-        if (output == demoKeyMsg) {
-            throw new Error('Key wasnt changed from default');
-        } else if (!output.match(/^Key set/)) {
-            throw new Error('Output message doesnt suggest key was set');
-        }
-    });
+  if (!nameOnCard) throw new Error("Envvar E2E_NAME_ON_CARD must be set.");
+  if (!customerId) throw new Error("Envvar E2E_CUSTOMER_ID must be set.");
+  if (!orderId) throw new Error("Envvar E2E_ORDER_ID must be set.");
+
+  console.log('Publishable Token:', publicApiToken);
+  console.log('RSA Key:', rsaKey);
+  console.log('Name on card:', nameOnCard);
+  console.log('CustomerId:', customerId);
+  console.log('OrderId:', orderId);
+
+  it('loads the page', function() {
+    browser.url('/pages/scripttag/index.html');
+
+    browser.pause(1000); // Wait for commonjs init to complete
+
+    browser.setValue('#pubToken', publicApiToken);
+    browser.click('#setPublicAPIKey');
+
+    browser.setValue('#rsaKey', rsaKey);
+    browser.click('#setRSAKey');
+
+    browser.setValue('#customerId', customerId);
+    browser.click('#setCustomerId');
+
+    browser.setValue('#customerId', customerId);
+    browser.click('#setCustomerId');
+
+    browser.setValue('#orderId', orderId);
+    browser.click('#setOrderId');
+  });
+
+  // Our first test populates the card encryption form, saves the ciphertext and submits the paymentMethodCipher
+  // for decryption, verifying that the card details saved against the customer match the encrypted inputs.
+  describe('returns the encrypted card details', function() {
 
     var ciphertext;
 
     it('encrypts the cardholder data', function() {
-        // Encrypt the card data.
-        browser.setValue('#name', nameOnCard);
-        browser.setValue('#number', '4111 1111 1111 1111');
-        browser.selectByValue('#month', '4')
-        browser.setValue('#year', '2019');
-        browser.click('#update');
+      // Encrypt the card data.
+      browser.setValue('#name', nameOnCard);
+      browser.setValue('#number', '4111 1111 1111 1111');
+      browser.selectByValue('#month', '4')
+      browser.setValue('#year', '2019');
+      browser.click('#encrypt');
 
-        // Check there wasn't an error.
-        raise(browser.getText('#output-error'));
+      // Check there wasn't an error.
+      raise(browser.getText('#encryptionOutputError'));
 
-        ciphertext = browser.getText('#output');
-        if (!ciphertext.match(/^\{.+\}$/) || !ciphertext.includes("cardCiphertext")) {
-            throw new Error("Doesn't look like we got a valid ciphertext: " + ciphertext);
-        }
+      ciphertext = browser.getText('#encryptionOutput');
+      if (!ciphertext.match(/^\{.+\}$/) || !ciphertext.includes("cardCiphertext")) {
+        throw new Error("Doesn't look like we got a valid ciphertext: " + ciphertext);
+      }
     });
 
     it('writes the cipher to a local file', function() {
-        require('fs').writeFileSync(outputFilepath, ciphertext || '');
+      require('fs').writeFileSync(ciphertextOutputFile, ciphertext || '');
     });
+  });
+
+  // Our second test submits a device fingerprint to the API under a random customerId. Using that same
+  // customerId, we read out their devices and assert that a device with our ravelinjs deviceId exists.
+  describe('ravelinjs tracks and fingerprints the browser', function() {
+
+    it('fingerprints the device and submits details + deviceId to Ravelin', function() {
+      browser.click('#trackFingerprint');
+
+      browser.pause(1000); // Wait for async request to complete
+
+      var error = browser.getText('#fingerprintError');
+      if (error) throw new Error(error);
+    });
+
+    it('writes the deviceId artifact', function() {
+      require('fs').writeFileSync(deviceIdFile, browser.getText('#deviceId') || '');
+    });
+  });
+
+  // Our final test triggers a track event for the 'vanilla', 'page', 'login' and 'logout' events.
+  // Using the same customerId and orderId, we will verify each of these 4 events are recorded against this
+  // session in Ravelin.
+  describe('ravelinjs tracks session activity', function() {
+
+    it('tracks custom events', function() {
+      browser.click('#track');
+
+      browser.pause(1000); // Wait for async request to complete
+
+      var error = browser.getText('#trackingError');
+      if (error) throw new Error(error);
+    });
+
+    it('tracks page load', function() {
+      browser.click('#trackPage');
+
+      browser.pause(1000); // Wait for async request to complete
+
+      var error = browser.getText('#trackingError');
+      if (error) throw new Error(error);
+    });
+
+    it('tracks login', function() {
+      browser.click('#trackLogin');
+
+      browser.pause(1000); // Wait for async request to complete
+
+      var error = browser.getText('#trackingError');
+      if (error) throw new Error(error);
+    });
+
+    it('tracks logout', function() {
+      browser.click('#trackLogout');
+
+      browser.pause(1000); // Wait for async request to complete
+
+      var error = browser.getText('#trackingError');
+      if (error) throw new Error(error);
+    });
+
+    it('writes the sessionId artifact', function() {
+      require('fs').writeFileSync(sessionIdFile, browser.getText('#sessionId') || '');
+    });
+  });
 });
 
 function raise(err) {
-    if (err) throw new Error(err);
+  if (err) throw new Error(err);
 }
