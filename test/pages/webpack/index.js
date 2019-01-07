55b2c5d2ac86b5c2f5d1f256e44d1861866f683a
sHesl <sHesl@users.noreply.github.com>
2019-01-07 12:38:08 +0000

    Extend crossbrowser tests to fingerprinting/session tracking

--- test/pages/webpack/index.js
+++ test/pages/webpack/index.js
@@ -1,16 +1,4 @@
 import ravelin from '../../../ravelin.js';
-import output from '../common.js';
+import TestPageSetup from '../common.js';
 
-ravelin.setRSAKey('10001|BB2D2D2FD3812FEBECF8955843228A0E1952342583DFC02B8393475C414E16FDCBE8753BD63C164104785D8A67E344D495B5C0C622CE8D643F3191BC6BE0D3050F391F77E1D7E1B8F69DA34B308477E31F775CCC44158E33FD7DDD51AC87DD33AD80B9B1BF850CEC79A189F011C0689C36C0C91BF6DB9CD65BB7399710E32D9876C00DD44103E54A64A44BF427E1BA4F48DA7AF3D623DBCCF282ED8D4CCAE31B921A9BE92F9E8D7B5C50FBD89D828539CAE3E3493D4F6D7ADA19A876D9DF3801B5C3CFFA6A3C72A246150F307D789BAD6E2408DA5EF05EE805E11C133FFEDFA57CD1C35E49106ADDAC43C51995B9C318066C9ACB4042D8A534370D79F1BAD601');
-document.getElementById('form').onsubmit = function() {
-    var month = document.getElementById('month');
-    output(function() {
-        return ravelin.encrypt({
-            nameOnCard: document.getElementById('name').value,
-            pan: document.getElementById('number').value,
-            month: month.options[month.selectedIndex].value,
-            year: document.getElementById('year').value,
-        });
-    });
-    return false;
-};
+new TestPageSetup(ravelin);
--- test/specs/test.js
+++ test/specs/test.js
@@ -1,48 +1,94 @@
 describe('ravelinjs', function() {
-    const cap = browser.desiredCapabilities;
+  const cap = browser.desiredCapabilities;
 
-    it('can be used with a script tag', function() {
-        browser.url('/pages/scripttag/index.html');
-        suite(browser);
-    });
+  it('can be used with a script tag', function() {
+    browser.url('/pages/scripttag/index.html');
+    suite(browser);
+  });
 
-    it('can be used minified with a script tag', function() {
-        browser.url('/pages/scripttag-min/index.html');
-        suite(browser);
-    });
+  it('can be used minified with a script tag', function() {
+    browser.url('/pages/scripttag-min/index.html');
+    suite(browser);
+  });
 
-    usuallyIt(!cap.requireJSTestDisabled, 'can be used with requirejs', function() {
-        browser.url('/pages/amd/index.html');
-        suite(browser);
-    });
+  usuallyIt(!cap.requireJSTestDisabled, 'can be used with requirejs', function() {
+    browser.url('/pages/amd/index.html');
+    suite(browser);
+  });
 
-    usuallyIt(!cap.requireJSTestDisabled, 'can be used minified with requirejs', function() {
-        browser.url('/pages/amd-min/index.html');
-        suite(browser);
-    });
+  usuallyIt(!cap.requireJSTestDisabled, 'can be used minified with requirejs', function() {
+    browser.url('/pages/amd-min/index.html');
+    suite(browser);
+  });
 
-    usuallyIt(!cap.webpackTestDisabled, 'can be used with webpack', function() {
-        browser.url('/pages/webpack/index.html');
-        suite(browser);
-    });
+  usuallyIt(!cap.webpackTestDisabled, 'can be used with webpack', function() {
+    browser.url('/pages/webpack/index.html');
+    suite(browser);
+  });
 });
 
 function suite(browser) {
-    // Do the form.
-    browser.setValue('#name', 'John');
-    browser.setValue('#number', '4111 1111 1111 1111');
-    browser.selectByValue('#month', '4')
-    browser.setValue('#year', '2019');
-    browser.click('#update');
+  checkIdsAreSet(browser);
+  checkCardEncryptionWorks(browser);
+  checkFingerprintingDoesNotError(browser);
+  checkTrackingEventsDoNotError(browser);
+}
 
-    // Check there was no error.
-    var error = browser.getText('#output-error');
+function checkIdsAreSet(browser) {
+  // Ensure that deviceId/sessionId are set upon lib instantiation
+  browser.getText('#deviceId').should.not.be.empty;
+  browser.getText('#sessionId').should.not.be.empty;
+  browser.getText('#cookies').should.not.be.empty;
+}
+
+function checkCardEncryptionWorks(browser) {
+  // Fill in the card encryption form
+  browser.setValue('#name', 'John');
+  browser.setValue('#number', '4111 1111 1111 1111');
+  browser.selectByValue('#month', '4')
+  browser.setValue('#year', '2019');
+  browser.click('#encrypt');
+
+  browser.pause(1000);
+
+  // Check there was no error
+  var error = browser.getText('#encryptionOutputError');
+  if (error) throw new Error(error);
+
+  // Check the result looked valid
+  browser.getText('#encryptionOutput').should.not.be.empty;
+}
+
+function checkFingerprintingDoesNotError(browser) {
+  // We have hooked up the #trackFingerprint button to call ravelinjs.trackFingerprint
+  // and provided a callback that writes any resulting errors to #fingerprintError.
+  // No text in #fingerprintError means no error occured fingerprinting the device.
+  // Reference common.js to check out these registrations.
+  browser.click('#trackFingerprint');
+
+  browser.pause(1000);
+
+  var error = browser.getText('#fingerprintError');
+  if (error) throw new Error(error);
+}
+
+function checkTrackingEventsDoNotError(browser) {
+  // We have hooked up the #track, #trackPage, #trackLogin and #trackLogout buttons to call
+  // their respective ravelinjs functions,  and provided a callback that writes any errors to #trackingError.
+  // No text in #trackingError means no error occured fingerprinting the device.
+  // Reference common.js to check out these registrations.
+  var buttons = ['', 'Page', 'Logout']; // Login is not exposed in ravelinjs yet
+
+  for (var i = 0; i < buttons.length; i++) {
+    browser.click('#track' + buttons[i]);
+
+    browser.pause(1000);
+
+    var error = browser.getText('#trackingError');
     if (error) throw new Error(error);
-
-    // Check the result looked valid.
-    browser.getText('#output').should.not.be.empty;
+  }
 }
 
 function usuallyIt(itDoes) {
-    return (itDoes ? it : it.skip).apply(this, Array.prototype.slice.call(arguments, 1));
+  return (itDoes ? it : it.skip).apply(this, Array.prototype.slice.call(arguments, 1));
 }
