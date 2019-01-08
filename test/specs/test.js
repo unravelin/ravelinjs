12a1fa121fb34f3ce9fe8b1240c4b01696b3201f
sHesl <sHesl@users.noreply.github.com>
2019-01-08 11:19:17 +0000

    No point in adding new cbt tests if they aren't really testing much

--- test/specs/test.js
+++ test/specs/test.js
@@ -2,91 +2,45 @@ describe('ravelinjs', function() {
   const cap = browser.desiredCapabilities;
 
   it('can be used with a script tag', function() {
-    browser.url('/pages/scripttag/index.html');
-    suite(browser);
+      browser.url('/pages/scripttag/index.html');
+      suite(browser);
   });
 
   it('can be used minified with a script tag', function() {
-    browser.url('/pages/scripttag-min/index.html');
-    suite(browser);
+      browser.url('/pages/scripttag-min/index.html');
+      suite(browser);
   });
 
   usuallyIt(!cap.requireJSTestDisabled, 'can be used with requirejs', function() {
-    browser.url('/pages/amd/index.html');
-    suite(browser);
+      browser.url('/pages/amd/index.html');
+      suite(browser);
   });
 
   usuallyIt(!cap.requireJSTestDisabled, 'can be used minified with requirejs', function() {
-    browser.url('/pages/amd-min/index.html');
-    suite(browser);
+      browser.url('/pages/amd-min/index.html');
+      suite(browser);
   });
 
   usuallyIt(!cap.webpackTestDisabled, 'can be used with webpack', function() {
-    browser.url('/pages/webpack/index.html');
-    suite(browser);
+      browser.url('/pages/webpack/index.html');
+      suite(browser);
   });
 });
 
 function suite(browser) {
-  checkCardEncryptionWorks(browser);
-  checkFingerprintingDoesNotError(browser);
-  checkTrackingEventsDoNotError(browser);
-  checkIdsAreSet(browser); // Check ids last because we have a hack the fingerprint test to ensure they exist
-}
-
-function checkIdsAreSet(browser) {
-  // Ensure that deviceId/sessionId are set upon lib instantiation
-  browser.getText('#deviceId').should.not.be.empty;
-  browser.getText('#sessionId').should.not.be.empty;
-  browser.getText('#cookies').should.not.be.empty;
-}
-
-function checkCardEncryptionWorks(browser) {
-  // Fill in the card encryption form
+  // Do the form.
   browser.setValue('#name', 'John');
   browser.setValue('#number', '4111 1111 1111 1111');
   browser.selectByValue('#month', '4')
   browser.setValue('#year', '2019');
-  browser.click('#encrypt');
+  browser.click('#update');
 
-  browser.pause(1000);
-
-  // Check there was no error
-  var error = browser.getText('#encryptionOutputError');
+  // Check there was no error.
+  var error = browser.getText('#output-error');
   if (error) throw new Error(error);
 
-  // Check the result looked valid
-  browser.getText('#encryptionOutput').should.not.be.empty;
-}
-
-function checkFingerprintingDoesNotError(browser) {
-  // We have hooked up the #trackFingerprint button to call ravelinjs.trackFingerprint
-  // and provided a callback that writes any resulting errors to #fingerprintError.
-  // No text in #fingerprintError means no error occured fingerprinting the device.
-  // Reference common.js to check out these registrations.
-  browser.click('#trackFingerprint');
-
-  browser.pause(1000);
-
-  var error = browser.getText('#fingerprintError');
-  if (error) throw new Error(error);
-}
-
-function checkTrackingEventsDoNotError(browser) {
-  // We have hooked up the #track, #trackPage, #trackLogin and #trackLogout buttons to call
-  // their respective ravelinjs functions,  and provided a callback that writes any errors to #trackingError.
-  // No text in #trackingError means no error occured fingerprinting the device.
-  // Reference common.js to check out these registrations.
-  var buttons = ['', 'Page', 'Logout']; // Login is not exposed in ravelinjs yet
-
-  for (var i = 0; i < buttons.length; i++) {
-    browser.click('#track' + buttons[i]);
-
-    browser.pause(1000);
-
-    var error = browser.getText('#trackingError');
-    if (error) throw new Error(error);
-  }
+  // Check the result looked valid.
+  browser.getText('#output').should.not.be.empty;
 }
 
 function usuallyIt(itDoes) {
