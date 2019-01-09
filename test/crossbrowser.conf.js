36cf8e8eb7942be3cb157840a33649538530b485
sHesl <sHesl@users.noreply.github.com>
2019-01-09 16:55:42 +0000

    Add chrome and osx test back in

--- test/crossbrowser.conf.js
+++ test/crossbrowser.conf.js
@@ -29,13 +29,13 @@ exports.config = Object.assign(base, {
             version: 'latest',
             platform: 'Windows 10',
         },
-        // {
-        //     build: 'ravelinjs 1.0',
-        //     name: 'osx chromeLatest',
-        //     browserName: 'Chrome',
-        //     version: 'latest',
-        //     platform: 'Mac OSX 10.12',
-        // },
+        {
+            build: 'ravelinjs 1.0',
+            name: 'osx chromeLatest',
+            browserName: 'Chrome',
+            version: 'latest',
+            platform: 'Mac OSX 10.12',
+        },
         {
             build: 'ravelinjs 1.0',
             name: 'win8 chrome18',
@@ -44,27 +44,20 @@ exports.config = Object.assign(base, {
             platform: 'Windows 8',
         },
         // Safari.
-        // {
-        //     build: 'ravelinjs 1.0',
-        //     name: 'osx safari11',
-        //     browserName: 'Safari',
-        //     version: '11',
-        //     platform: 'Mac OSX 10.13',
-        // },
-        // {
-        //     build: 'ravelinjs 1.0',
-        //     name: 'osx safari10',
-        //     browserName: 'Safari',
-        //     version: '10',
-        //     platform: 'Mac OSX 10.12',
-        // },
-        // {
-        //     build: 'ravelinjs 1.0',
-        //     name: 'osx safari6',
-        //     browserName: 'Safari',
-        //     version: '6.2',
-        //     platform: 'Mac OSX 10.8',
-        // },
+        {
+            build: 'ravelinjs 1.0',
+            name: 'osx safari11',
+            browserName: 'Safari',
+            version: '11',
+            platform: 'Mac OSX 10.13',
+        },
+        {
+            build: 'ravelinjs 1.0',
+            name: 'osx safari10',
+            browserName: 'Safari',
+            version: '10',
+            platform: 'Mac OSX 10.12',
+        },
         // Firefox.
         {
             build: 'ravelinjs 1.0',
