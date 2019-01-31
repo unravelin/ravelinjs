764a24c8b7a8e41d8e2fdf8306cd252d76278c47
sHesl <sHesl@users.noreply.github.com>
2019-01-31 13:41:42 +0000

    Remove AMD loader from sjcl!

--- test/specs/test.js
+++ test/specs/test.js
@@ -2,55 +2,61 @@ describe('ravelinjs', function() {
   const cap = browser.desiredCapabilities;
 
   it('can be used with a script tag', function() {
-    runSuiteWithOneRetry(browser, '/pages/scripttag/index.html');
+    runSuiteWithOneRetry('/pages/scripttag/index.html');
   });
 
   it('can be used minified with a script tag', function() {
-    runSuiteWithOneRetry(browser, '/pages/scripttag-min/index.html');
+    runSuiteWithOneRetry('/pages/scripttag-min/index.html');
   });
 
   usuallyIt(!cap.requireJSTestDisabled, 'can be used with requirejs', function() {
-    runSuiteWithOneRetry(browser, '/pages/amd/index.html');
+    runSuiteWithOneRetry('/pages/amd/index.html');
   });
 
   usuallyIt(!cap.requireJSTestDisabled, 'can be used minified with requirejs', function() {
-    runSuiteWithOneRetry(browser, '/pages/amd-min/index.html');
+    runSuiteWithOneRetry('/pages/amd-min/index.html');
   });
 
   usuallyIt(!cap.webpackTestDisabled, 'can be used with webpack', function() {
-    runSuiteWithOneRetry(browser, '/pages/webpack/index.html');
+    runSuiteWithOneRetry('/pages/webpack/index.html');
   });
 });
 
-function suite(browser) {
-  checkIdsAreSet(browser);
-  checkCardEncryptionWorks(browser);
-  checkFingerprintingDoesNotError(browser);
-  checkTrackingEventsDoNotError(browser);
-}
 
-function runSuiteWithOneRetry(browser, page) {
+function runSuiteWithOneRetry(page) {
   try {
     browser.url(page);
     browser.pause(500);
-    suite(browser);
+    suite();
   } catch(e) {
     browser.url(page);
     browser.pause(500);
-    suite(browser);
+    suite();
   }
 }
 
-function checkIdsAreSet(browser) {
-  // Ensure that deviceId/sessionId are set upon lib instantiation
-  console.log(document.cookie);
+function suite() {
+  checkCookiesAreSet();
+  checkCardEncryptionWorks();
+  checkFingerprintingDoesNotError();
+  checkTrackingEventsDoNotError();
 }
 
-function checkCardEncryptionWorks(browser) {
+function checkCookiesAreSet() {
+  if (!browser.getCookie('ravelinDeviceId')) {
+    throw new Error('Expected cookie "ravelinDeviceId" to be set');
+  }
+
+  if (!browser.getCookie('ravelinSessionId')) {
+    throw new Error('Expected cookie "ravelinSessionId" to be set');
+  }
+}
+
+function checkCardEncryptionWorks() {
   // Fill in the card encryption form
   browser.setValue('#name', 'John');
   browser.setValue('#number', '4111 1111 1111 1111');
-  browser.selectByValue('#month', '4')
+  browser.selectByValue('#month', '4');
   browser.setValue('#year', '2019');
   browser.click('#encrypt');
 
