b5ae6b2549c14234b5784e0d3b61960c50ed106e
Paul Scott <paul.scott@ravelin.com>
2019-05-28 11:20:00 +0100

    Use Edge 18 instead of Edge 14.
    crossbrowsertesting.com says in its capabilities inspector that Edge 14
    on Windows 10 is still an option, but the latest build shows 'No such
    driver' errors.
    
    https://circleci.com/gh/unravelin/ravelinjs/48639

--- test/crossbrowser.conf.js
+++ test/crossbrowser.conf.js
@@ -93,9 +93,9 @@ exports.config = Object.assign(base, {
         // Edge.
         {
             build: 'ravelinjs 1.0',
-            name: 'win10 edge14',
+            name: 'win10 edge18',
             browserName: 'MicrosoftEdge',
-            version: '14',
+            version: '18',
             platform: 'Windows 10',
             screenResolution: '1366x768',
         },
