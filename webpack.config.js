082e17f8df907144e1824909b4d14f5667c9ee1b
Rob Crocombe <rjwc1993@yahoo.co.uk>
2019-09-27 17:03:55 +0100

    Add minified version to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,40 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  devtool: false,
-  entry: {
+module.exports = function(env, argv) {
+  const production = env === 'production';
+  const entry = {
     'ravelin': path.resolve(__dirname, 'src/ravelin.js'),
     'ravelin-no-encrypt': path.resolve(__dirname, 'src/ravelin-no-encrypt.js'),
-  },
-  output: {
-    pathinfo: false,
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    library: 'ravelinjs',
-    libraryTarget: 'umd',
-  },
+  };
+
+  if (production) {
+    entry['ravelin.min'] = entry['ravelin'];
+    entry['ravelin-no-encrypt.min'] = entry['ravelin-no-encrypt'];
+  }
+
+  return {
+    mode: production ? 'production' : 'development',
+    devtool: false,
+    entry: entry,
+    output: {
+      pathinfo: false,
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+      library: 'ravelinjs',
+      libraryTarget: 'umd',
+    },
+    optimization: {
+      minimize: production,
+      minimizer: [
+        new TerserPlugin({
+          include: /\.min\.js$/,
+          terserOptions: {
+            ie8: true,
+            safari10: true,
+          },
+        }),
+      ],
+    },
+  };
 };
