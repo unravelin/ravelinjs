f0509e47bfd1102763dd6b3a1db787af6e4a249b
sHesl <sHesl@users.noreply.github.com>
2019-10-21 14:50:51 +0100

    Version

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 // Versioning
-var RAVELINJS_VERSION = '1.0.0';
+var RAVELINJS_VERSION = require('../package.json').version;
 var FULL_VERSION_STRING = RAVELINJS_VERSION + '-ravelinjs';
 
 module.exports = FULL_VERSION_STRING;
