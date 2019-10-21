b22739a875275742954a92a483c7f3f302cb5ae2
sHesl <sHesl@users.noreply.github.com>
2019-10-21 14:51:23 +0100

    Vendored code to own file

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const sinon = require('sinon');
 
 const dummyRSAKey = '10001|BB2D2D2FD3812FEBECF8955843228A0E1952342583DFC02B8393475C414E16FDCBE8753BD63C164104785D8A67E344D495B5C0C622CE8D643F3191BC6BE0D3050F391F77E1D7E1B8F69DA34B308477E31F775CCC44158E33FD7DDD51AC87DD33AD80B9B1BF850CEC79A189F011C0689C36C0C91BF6DB9CD65BB7399710E32D9876C00DD44103E54A64A44BF427E1BA4F48DA7AF3D623DBCCF282ED8D4CCAE31B921A9BE92F9E8D7B5C50FBD89D828539CAE3E3493D4F6D7ADA19A876D9DF3801B5C3CFFA6A3C72A246150F307D789BAD6E2408DA5EF05EE805E11C133FFEDFA57CD1C35E49106ADDAC43C51995B9C318066C9ACB4042D8A534370D79F1BAD601';
 const dummyRSAKeyWithIndex = '2|10001|BB2D2D2FD3812FEBECF8955843228A0E1952342583DFC02B8393475C414E16FDCBE8753BD63C164104785D8A67E344D495B5C0C622CE8D643F3191BC6BE0D3050F391F77E1D7E1B8F69DA34B308477E31F775CCC44158E33FD7DDD51AC87DD33AD80B9B1BF850CEC79A189F011C0689C36C0C91BF6DB9CD65BB7399710E32D9876C00DD44103E54A64A44BF427E1BA4F48DA7AF3D623DBCCF282ED8D4CCAE31B921A9BE92F9E8D7B5C50FBD89D828539CAE3E3493D4F6D7ADA19A876D9DF3801B5C3CFFA6A3C72A246150F307D789BAD6E2408DA5EF05EE805E11C133FFEDFA57CD1C35E49106ADDAC43C51995B9C318066C9ACB4042D8A534370D79F1BAD601';
-const expectedVersion = '1.0.0-ravelinjs';
+const expectedVersion = '0.1.0-ravelinjs';
 
 describe('ravelin.js', function() {
   var ravelin;
@@ -11,7 +11,6 @@ describe('ravelin.js', function() {
   function reset() {
     delete require.cache[require.resolve('../src/core')];
     delete require.cache[require.resolve('../src/encryption')];
-    delete require.cache[require.resolve('../src/init')];
     delete require.cache[require.resolve('../src/version')];
     delete require.cache[require.resolve('../src/ravelin')];
     ravelin = require('../src/ravelin');
@@ -316,6 +315,14 @@ describe('ravelin.js', function() {
       expect(outputA).to.satisfy(assertCipher);
       expect(outputB).to.satisfy(assertCipher);
       expect(outputA).not.to.equal(outputB);
+    });
+
+    it('does not expose sjcl or RSAKey', function() {
+      expect(() => expect(sjcl)).to.throw('sjcl is not defined');
+      expect(() => expect(RSAKey)).to.throw('RSAKey is not defined');
+
+      expect(ravelin.sjcl).to.be.undefined;
+      expect(ravelin.RSAKey).to.be.undefined;
     });
   });
 
@@ -790,7 +797,6 @@ describe('ravelin-no-encrypt.js', function() {
 
   function reset() {
     delete require.cache[require.resolve('../src/core')];
-    delete require.cache[require.resolve('../src/init')];
     delete require.cache[require.resolve('../src/version')];
     delete require.cache[require.resolve('../src/ravelin-no-encrypt')];
     ravelin = require('../src/ravelin-no-encrypt');
@@ -905,7 +911,7 @@ function assertCipher(c) {
          c.cardCiphertext != '' && c.cardCiphertext.length > 10 &&
          c.aesKeyCiphertext != '' && c.aesKeyCiphertext.length > 10 &&
          c.algorithm == 'RSA_WITH_AES_256_GCM' &&
-         c.ravelinSDKVersion == '1.0.0-ravelinjs' &&
+         c.ravelinSDKVersion == '0.1.0-ravelinjs' &&
          typeof(c.keyIndex) === 'number';
 }
 
