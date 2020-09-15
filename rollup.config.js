f5ddd74ef118f61f3cdfe03ff9eb714e74428f55
Paul Scott <paul.scott@ravelin.com>
2020-09-15 18:42:47 +0100

    minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,39 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
+import { terser } from 'rollup-plugin-terser';
 import { basename } from 'path';
 import glob from 'glob';
 
-const bundles = glob.sync("lib/bundle/*.js");
+var builds = module.exports = [];
 
-export default bundles.map(bundle => (
+var output = {
+  format: 'iife',
+  name: 'ravelinjs'
+};
+var plugins = [
+  replace({
+    'process.env.npm_package_version': JSON.stringify(require('./package.json').version),
+  }),
+  resolve(),
+  commonjs(),
+];
+
+glob.sync("lib/bundle/*.js").forEach(bundle => builds.push(
   {
     input: bundle,
     output: {
       file: 'build/ravelin-' + basename(bundle),
-      format: 'iife',
-      name: 'ravelinjs'
+      ...output,
     },
-    plugins: [
-      replace({
-        'process.env.npm_package_version': JSON.stringify(require('./package.json').version),
-      }),
-      resolve(),
-      commonjs(),
-    ]
-  }
+    plugins: plugins,
+  },
+  {
+    input: bundle,
+    output: {
+      file: 'build/ravelin-' + basename(bundle).replace(/\.js$/, '.min.js'),
+      ...output,
+    },
+    plugins: plugins.concat([terser()]),
+  },
 ));
