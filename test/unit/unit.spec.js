1c692ed8c3cd44d087c645dfacf84575ac537ce5
Paul Scott <paul.scott@ravelin.com>
2020-09-23 17:30:26 +0100

    unit.spec: Wait for finish and report test failures.

--- test/unit/unit.spec.js
+++ test/unit/unit.spec.js
@@ -4,12 +4,19 @@ describe('ravelinjs unit tests', function() {
     expect(browser).toHaveTitleContaining('Mocha');
 
     browser.waitUntil(function() {
-      return $('#mocha-stats .duration').getText() !== 'duration: 0';
+      // The unit test page will assign this ID to an element when the final
+      // test has run.
+      return !!$('#completed');
     });
 
-    var stats = $('#mocha-stats').getText();
-    if (stats.indexOf('failures: 0') === -1) {
-      throw new Error('Stats: ' + stats.replace(/(\d)([fd])/g, '$1, $2'));
+    // See if we got any errors.
+    var stats = $('#mocha-stats').getText().replace(/(\d)([fd])/g, '$1, $2');
+    if (stats.indexOf('failures: 0,') !== -1) {
+      return;
     }
+
+    // Report back errors.
+    var failures = $('#completed pre.error').getText();
+    throw new Error('Stats: ' + stats + '. ' + failures);
   });
 });
