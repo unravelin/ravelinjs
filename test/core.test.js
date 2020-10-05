826513321110db3f7dbac7291600926b3cc590a8
Paul Scott <paul.scott@ravelin.com>
2020-10-05 12:07:23 +0100

    core.test: Organise under #methods.

--- test/core.test.js
+++ test/core.test.js
@@ -6,37 +6,39 @@ describe('ravelin.core', function() {
     deleteAllCookies();
   });
 
-  it('returns IDs', function() {
-    var r = new Ravelin({});
-    return r.core.id().then(function(id) {
-      expect(id).to.match(/rjs-[a-z0-9-]{30,}/);
-    });
-  });
-
-  it('keeps returning the same ID', function() {
-    var r = new Ravelin({});
-    return r.core.id().then(function(id1) {
-      return r.core.id().then(function(id2) {
-        expect(id1).to.eql(id2);
+  describe('#id', function() {
+    it('returns IDs', function() {
+      var r = new Ravelin({});
+      return r.core.id().then(function(id) {
+        expect(id).to.match(/rjs-[a-z0-9-]{30,}/);
       });
     });
-  });
 
-  it('reinstates an ID removed from cookies', function() {
-    var r1 = new Ravelin({
-      syncMS: 30
-    });
-    return r1.core.id().then(function(id1) {
-      // Take the ID out of the cookies.
-      deleteAllCookies();
-
-      // Wait a second.
-      return new r1.core.Promise(function(resolve) {
-        setTimeout(resolve, 100);
-      }).then(function() {
-        var r2 = new Ravelin({});
-        return r2.core.id().then(function(id2) {
+    it('keeps returning the same ID', function() {
+      var r = new Ravelin({});
+      return r.core.id().then(function(id1) {
+        return r.core.id().then(function(id2) {
           expect(id1).to.eql(id2);
+        });
+      });
+    });
+
+    it('reinstates an ID removed from cookies', function() {
+      var r1 = new Ravelin({
+        syncMS: 30
+      });
+      return r1.core.id().then(function(id1) {
+        // Take the ID out of the cookies.
+        deleteAllCookies();
+
+        // Wait a second.
+        return new r1.core.Promise(function(resolve) {
+          setTimeout(resolve, 100);
+        }).then(function() {
+          var r2 = new Ravelin({});
+          return r2.core.id().then(function(id2) {
+            expect(id1).to.eql(id2);
+          });
         });
       });
     });
@@ -54,7 +56,7 @@ describe('ravelin.core', function() {
     ];
     for (var i = 0; i < tests.length; i++) {
       (function(test) {
-        it('infers API ' + test.api + ' from key ' + test.key, function() {
+        it('infers url from key in test = ' + JSON.stringify(test), function() {
           xhook.before(function(req) {
             expect(req.url).to.be(test.url + '?key=' + test.key);
             return {status: 204, text: ""};
@@ -66,91 +68,93 @@ describe('ravelin.core', function() {
     }
   });
 
-  it('returns a rejected promise for bad bodies', function() {
-    // Create b with a cyclical reference to itself, which will fail to stringify.
-    var b = {};
-    b.b = b;
+  describe('#send', function() {
+    it('returns a rejected promise for bad bodies', function() {
+      // Create b with a cyclical reference to itself, which will fail to stringify.
+      var b = {};
+      b.b = b;
 
-    // Try to serialise b in a request.
-    var r = new Ravelin({key: 'k', api: '/'});
-    try {
-      return r.core.send('POST', 'z', b)
-        .then(
-          function(r) {
-            throw new Error('Expected an error but got result: ' + JSON.stringify(r));
+      // Try to serialise b in a request.
+      var r = new Ravelin({key: 'k', api: '/'});
+      try {
+        return r.core.send('POST', 'z', b)
+          .then(
+            function(r) {
+              throw new Error('Expected an error but got result: ' + JSON.stringify(r));
+            },
+            function(e) {
+              expect(e).to.be.a(TypeError);
+            }
+          );
+      } catch (e) {
+        throw new Error('Expected failed promise but caught exception: ' + e.message);
+      }
+    });
+
+    it('wont retry unknown errors', function() {
+      xhook.before(function(r) {
+        throw new Error('not retried');
+      });
+      var r = new Ravelin({key: 'k', api: '/'});
+      return r.core.send('POST', 'z', {}).
+        then(
+          function pass(r) {
+            throw new Error('Exception expected but got result ' + JSON.stringify(r));
           },
-          function(e) {
-            expect(e).to.be.a(TypeError);
+          function fail(err) {
+            expect(err).to.be.an(Error);
+            expect(err.message).to.eql('not retried');
           }
         );
-    } catch (e) {
-      throw new Error('Expected failed promise but caught exception: ' + e.message);
-    }
-  });
-
-  it('wont retry unknown errors', function() {
-    xhook.before(function(r) {
-      throw new Error('not retried');
     });
-    var r = new Ravelin({key: 'k', api: '/'});
-    return r.core.send('POST', 'z', {}).
-      then(
-        function pass(r) {
-          throw new Error('Exception expected but got result ' + JSON.stringify(r));
-        },
-        function fail(err) {
-          expect(err).to.be.an(Error);
-          expect(err.message).to.eql('not retried');
+
+    it('retries 500s', function() {
+      var failures = 0;
+      xhook.before(function(req) {
+        if (failures < 1) {
+          failures++;
+          return {status: 500};
         }
-      );
-  });
-
-  it('retries 500s', function() {
-    var failures = 0;
-    xhook.before(function(req) {
-      if (failures < 1) {
-        failures++;
-        return {status: 500};
-      }
-      return {status: 204};
-    });
-    var rav = new Ravelin({
-      api: '/',
-      key: 'retries'
-    });
-    return rav.core.send('POST', 'z', {
-      retryTest: 'hello'
-    }).then(function(r) {
-      expect(r).to.eql({
-        status: 204,
-        attempt: 2,
-        text: ""
+        return {status: 204};
       });
-    });
-  });
-
-  it('retries 500s at most 3 times', function() {
-    xhook.before(function(req) {
-      return {status: 500};
-    });
-    var rav = new Ravelin({
-      api: '/',
-      key: 'retries'
-    });
-    return rav.core.send('POST', 'z', {
-      retryTest: 'hello'
-    }).then(
-      function (r) {
-        throw r;
-      },
-      function(r) {
+      var rav = new Ravelin({
+        api: '/',
+        key: 'retries'
+      });
+      return rav.core.send('POST', 'z', {
+        retryTest: 'hello'
+      }).then(function(r) {
         expect(r).to.eql({
-          status: 500,
-          attempt: 3,
+          status: 204,
+          attempt: 2,
           text: ""
         });
-      }
-    );
+      });
+    });
+
+    it('retries 500s at most 3 times', function() {
+      xhook.before(function(req) {
+        return {status: 500};
+      });
+      var rav = new Ravelin({
+        api: '/',
+        key: 'retries'
+      });
+      return rav.core.send('POST', 'z', {
+        retryTest: 'hello'
+      }).then(
+        function (r) {
+          throw r;
+        },
+        function(r) {
+          expect(r).to.eql({
+            status: 500,
+            attempt: 3,
+            text: ""
+          });
+        }
+      );
+    });
   });
 });
 
