589276382dafb7231f0c513b60b3573c894d3b51
Dimitra Zuccarelli <dimitrazuccarelli@gmail.com>
2022-10-25 14:12:45 +0100

    Karma config

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,68 +1,60 @@
-const path = require('path');
+const path = require("path");
 
 // Karma configuration
 // Generated on Mon Sep 21 2020 19:31:34 GMT+0100 (British Summer Time)
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
-
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
+    basePath: "",
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'expect'],
-
+    frameworks: ["mocha", "expect"],
 
     // list of files / patterns to load in the browser
     files: [
-      'https://unpkg.com/jquery@1.12.4/dist/jquery.js',
-      path.resolve(path.join(__dirname, '../node_modules/xhook/dist/xhook.js')),
-      path.join(__dirname, '../build/ravelin-core+track+encrypt+promise.js'),
-      path.join(__dirname, 'common.js'),
-      path.join(__dirname, '**/*.test.js')
+      "https://unpkg.com/jquery@1.12.4/dist/jquery.js",
+      path.resolve(path.join(__dirname, "../node_modules/xhook/dist/xhook.js")),
+      path.join(__dirname, "../build/ravelin-core+track+encrypt+promise.js"),
+      path.join(__dirname, "common.js"),
+      path.join(__dirname, "**/*.test.js"),
     ],
-
 
     // list of files / patterns to exclude
-    exclude: [
-    ],
-
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
+    preprocessors: {},
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
+    reporters: ["progress"],
 
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
+    browsers: ["ChromeHeadlessNoSandbox"],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox"],
+      },
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -70,6 +62,6 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
   });
 };
