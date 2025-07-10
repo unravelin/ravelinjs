e19c308b87cf7bd1ce8a2bdcedea50fad4176456
Rob Crocombe <git@robcrocombe.com>
2025-07-10 18:50:25 +0100

    Handle server independently to avoid duplication issue
    https://automation.browserstack.com/projects/ravelinjs/builds/ci+dep-test/9?tab=tests&testListView=spec

--- test-integration/server-cli.mjs
+++ test-integration/server-cli.mjs
@@ -1,3 +1,8 @@
 import { startServer } from './server.mjs';
 
+// Assign the process title from the npm script args
+// This allows us to kill the process externally by name
+// https://stackoverflow.com/a/33922979
+process.title = process.argv[2];
+
 startServer();
