ac98f9d72209772fc43b126003acb8c9886b6305
Eric Matteucci <eric.matteucci@ravelin.com>
2026-01-19 13:48:42 +0000

    use rollup for deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
-import { terser } from 'rollup-plugin-terser';
-import license from 'rollup-plugin-license';
-import { basename } from 'path';
 import glob from 'glob';
+import { basename } from 'path';
+import license from 'rollup-plugin-license';
+import { terser } from 'rollup-plugin-terser';
 import packageJson from './package.json';
 
-var builds = module.exports = [];
+var builds = (module.exports = []);
 
 var output = {
   format: 'iife',
@@ -24,7 +24,7 @@ var output = {
 var plugins = [
   replace({
     preventAssignment: true,
-    'RAVELINJS_VERSION': JSON.stringify(packageJson.version + '-ravelinjs'),
+    RAVELINJS_VERSION: JSON.stringify(packageJson.version + '-ravelinjs'),
   }),
   resolve(),
   commonjs(),
@@ -33,48 +33,55 @@ var plugins = [
   }),
 ];
 
-glob.sync('lib/bundle/*.js')
-.sort((a, b) => b.length - a.length)
-.forEach(bundle => builds.push(
-  {
-    input: bundle,
-    output: {
-      file: 'build/ravelin-' + basename(bundle).replace(/\.js$/, '.min.js'),
-      ...output,
-    },
-    plugins: plugins.concat([
-      terser({
-        compress: {
-          typeofs: false,
+glob
+  .sync('lib/bundle/*.js')
+  .sort((a, b) => b.length - a.length)
+  .forEach((bundle) =>
+    builds.push(
+      {
+        input: bundle,
+        output: {
+          file: 'build/ravelin-' + basename(bundle).replace(/\.js$/, '.min.js'),
+          ...output,
         },
-        safari10: true,
-      }),
-    ]),
-  },
-  {
-    input: bundle,
-    output: {
-      file: 'build/ravelin-' + basename(bundle),
-      ...output,
-    },
-    plugins: plugins,
-  },
-  {
-    input: bundle,
-    output: {
-      file: 'dist/' + basename(bundle),
-      format: 'umd',
-      name: 'Ravelin',
-      esModule: false,
-      exports: 'default',
+        plugins: plugins.concat([
+          terser({
+            compress: {
+              typeofs: false,
+            },
+            safari10: true,
+          }),
+        ]),
+      },
+      {
+        input: bundle,
+        output: {
+          file: 'build/ravelin-' + basename(bundle),
+          ...output,
+        },
+        plugins: plugins,
+      },
+      {
+        input: bundle,
+        external: ['detectincognitojs'],
+        output: {
+          file: 'dist/' + basename(bundle),
+          format: 'umd',
+          name: 'Ravelin',
+          esModule: false,
+          exports: 'default',
+          globals: {
+            detectincognitojs: 'detectIncognito',
+          },
 
-      // Prevent Object.freeze being used for namespace references.
-      // https://www.rollupjs.org/guide/en/#outputfreeze.
-      freeze: false,
-      // Prevent Object.defineProperty being used for dynamic exports.
-      // https://www.rollupjs.org/guide/en/#outputexternallivebindings.
-      externalLiveBindings: false,
-    },
-    plugins: plugins,
-  }
-));
+          // Prevent Object.freeze being used for namespace references.
+          // https://www.rollupjs.org/guide/en/#outputfreeze.
+          freeze: false,
+          // Prevent Object.defineProperty being used for dynamic exports.
+          // https://www.rollupjs.org/guide/en/#outputexternallivebindings.
+          externalLiveBindings: false,
+        },
+        plugins: plugins,
+      }
+    )
+  );
