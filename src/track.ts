bb31506f9d2448f22c397d42bed0ccfcb40d008b
Eric Matteucci <eric.matteucci@ravelin.com>
2026-04-23 10:03:55 +0100

    Update comment on disable function

--- src/track.ts
+++ src/track.ts
@@ -77,7 +77,8 @@ export class Track {
   }
 
   /**
-   * Disable tracking by removing all event listeners.
+   * Disable tracking by removing all automatically attached event listeners,
+   * (paste and resize). Manually attached track events will still be sent.
    */
   public disable(): void {
     this._detach();
