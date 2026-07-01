7132df7cb37afe8ad7f50bab77e40e5d1031105b
Eric Matteucci <eric.matteucci@ravelin.com>
2026-07-01 10:39:29 +0100

    update precedence for webdriver detector

--- src/bot/detectors/automation/web-driver-detector.ts
+++ src/bot/detectors/automation/web-driver-detector.ts
@@ -8,7 +8,7 @@
  */
 export default class WebDriverDetector implements detection.Detector {
   public readonly signal = 'webdriver';
-  public readonly precedence = 100;
+  public readonly precedence = 10;
 
   public triggered = false;
   public indicators: string[] = [];
