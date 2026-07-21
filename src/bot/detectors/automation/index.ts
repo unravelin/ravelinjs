5e3435dcd46f9bb341cfde2533c205d7332e4a82
Eric Matteucci <eric.matteucci@ravelin.com>
2026-07-21 14:59:32 +0100

    add puppeteer detector

--- src/bot/detectors/automation/index.ts
+++ src/bot/detectors/automation/index.ts
@@ -1,9 +1,15 @@
 import PlaywrightDetector from './playwright-detector';
+import PuppeteerDetector from './puppeteer-detector';
 import SeleniumDetector from './selenium-detector';
 import WebDriverDetector from './webdriver-detector';
 
 export default function createAutomationDetectors(
   env: detection.Environment
 ): detection.Detector[] {
-  return [new WebDriverDetector(env), new PlaywrightDetector(env), new SeleniumDetector(env)];
+  return [
+    new WebDriverDetector(env),
+    new PlaywrightDetector(env),
+    new PuppeteerDetector(env),
+    new SeleniumDetector(env),
+  ];
 }
